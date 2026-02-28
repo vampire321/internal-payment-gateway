0fc75fd2b6a252c0022f37fd666d513e6a6e0c4a
vampire321 <mh1295551@gmail.com>
2026-02-28 16:01:05 +0530

    database layer is done

--- internal/database/postgres.go
+++ internal/database/postgres.go
@@ -17,10 +17,10 @@ func Connect(cfg *config.Config) (*sql.DB, error){
 		if err != nil {
 			return nil, fmt.Errorf("failed to open database connection: %w", err)
 		}
-}	
-	//verify connection to the database with a timeout context and with retry logic
+	
+	//verify connection to the database with a timeout context and with retry logic(docker safe)
 	ctx, Cancel := context.WithTimeout(context.Background(),10*time.Second)
-	defer cancel()
+	defer Cancel()
 	for{
 	err = db.PingContext(ctx)
 	if err == nil{
@@ -36,3 +36,32 @@ func Connect(cfg *config.Config) (*sql.DB, error){
 	db.SetMaxOpenConns(25)
 	db.SetMaxIdleConns(10)
 	db.SetConnMaxLifetime(5 * time.Minute)
+
+	if err := migrate(db); err != nil {
+		return nil, err
+	}
+	return db, nil
+}
+
+	func migrate(db *sql.DB) error {
+
+	query := `
+	CREATE TABLE IF NOT EXISTS payments (
+		id SERIAL PRIMARY KEY,
+		user_id TEXT NOT NULL,
+		amount NUMERIC NOT NULL,
+		currency TEXT NOT NULL,
+		status TEXT NOT NULL,
+		idempotency_key TEXT UNIQUE NOT NULL,
+		created_at TIMESTAMP DEFAULT NOW()
+	);
+	`
+	_, err := db.Exec(query)
+	if err != nil {
+		return fmt.Errorf("migration failed: %w", err)
+	}
+	return nil
+}
+
+
+	
